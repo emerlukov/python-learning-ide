8819603135abb5418bd777866836bd18ee96a742
emerlukov <emerlukov@gmail.com>
2026-06-11 09:47:51 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- file_manager.py
+++ file_manager.py
@@ -11,6 +11,7 @@ from kivy.utils import platform
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.graphics import Color, Rectangle
 import time
+from kivy.core.window import Window
 
 from kivymd.uix.label import MDIcon
 from kivymd.uix.button import MDRectangleFlatButton, MDIconButton
@@ -492,6 +493,9 @@ class FileBrowserPopup:
             self.filename_input.line_color_normal = PURPLE
             self.filename_input.text_color = PURPLE
             self.filename_input.hint_text_color = (0.6, 0.6, 0.6, 1)
+
+            self.filename_input.bind(focus=self._on_input_focus)
+
             content.add_widget(self.filename_input)
 
         btn_layout = MDBoxLayout(size_hint_y=None, height=dp(40), spacing=dp(8))
@@ -534,6 +538,15 @@ class FileBrowserPopup:
             self.app.wrap_widget_buttons(content)
 
         self._refresh_list()
+
+    def _on_input_focus(self, instance, value):
+        if value:
+            Clock.schedule_once(lambda dt: self._lift_popup(), 0.2)
+
+    def _lift_popup(self):
+        if self.popup and self.popup._window:
+            # Поднимаем попап на 40% высоты экрана вверх
+            self.popup.y += Window.height * 0.4
 
     def _update_bg(self, instance, value):
         if hasattr(self, '_bg_rect'):
